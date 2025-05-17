d74a85ce99fe9981d10fabc1842bc9586993c8e1
eduardafranca <mariaeduardafranca.silva@ucsal.edu.br>
2025-05-17 17:10:40 -0300

    meu primeiro projeto no gh

---
+++ src/main/java/org/yourcompany/yourproject/MeuProjetoDio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author eduar
+ */
+public class MeuProjetoDio {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, consegui colocar o projeto no meu primeiro repositorio");
+    }
+}
